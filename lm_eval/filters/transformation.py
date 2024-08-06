ed3aaad962a64d901b239b2a422a446616bcb937
maaxap <max.aparovich@gmail.com>
2024-08-07 01:49:39 +0200

    Added CastDtypeFilter

--- lm_eval/filters/transformation.py
+++ lm_eval/filters/transformation.py
@@ -1,3 +1,5 @@
+import builtins
+
 from lm_eval.api.filter import Filter
 from lm_eval.api.registry import register_filter
 
@@ -54,3 +56,40 @@ class MapFilter(Filter):
             return [self.mapping_dict.get(resp, self.default_value) for resp in inst]
 
         return [filter_set(resp) for resp in resps]
+
+
+@register_filter("cast_dtype")
+class CastDtypeFilter(Filter):
+    def __init__(self, dtype: str) -> None:
+        """
+        Initializes the CastDtypeFilter with a given data type.
+
+        Args:
+        - dtype (str): The data type to cast the responses to.
+                       Must be a valid built-in Python data type.
+
+        Example:
+        caster = CastDtypeFilter('int')
+        """
+        assert dtype in {
+            "int",
+            "float",
+            "str",
+            "bool",
+            "complex",
+            "bytes",
+            "bytearray",
+            "memoryview",
+            "list",
+            "tuple",
+            "set",
+            "frozenset",
+            "dict",
+        }, f"Provided dtype is not a valid built-in Python data type: {dtype}"
+        self.dtype = getattr(builtins, dtype, None)
+
+    def apply(self, resps, docs):
+        def cast_dtype(inst):
+            return [self.dtype(resp) for resp in inst]
+
+        return [cast_dtype(resp) for resp in resps]
